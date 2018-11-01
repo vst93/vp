e5e99e3c54a660cb5c6cb360c3f0faaf13485178
vst <vst93g@gmail.com>
2018-11-02 00:10:59 +0800

    1

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin-v
+ * Date: 2018/11/1
+ * Time: 23:05
+ */
+
+echo '{
+  "text" : "text, this field may accept markdown",
+  "attachments" : [
+    {
+      "title" : "title_1",
+      "text" : "attachment_text",
+      "color" : "#666666",
+      "images" : [
+        {
+          "url" : "http://example.com/index.jpg"
+        },
+        {
+          "url" : "http://example.com/index.jpg"
+        }
+      ]
+    }
+  ]
+}';
